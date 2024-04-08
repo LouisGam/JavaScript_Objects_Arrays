d2127a3ffd8b6b6d30f3f31cdd5600adbed00f61
Louis Gambardella <LouisGambardella@gmail.com>
2024-04-08 08:30:02 -0700

    objects exercise

--- app.js
+++ app.js
@@ -38,6 +38,63 @@ book.info = function() {
 return `${this.title} by ${this.author} has ${this.pages} pages. And has been read ${this.read} time(s)`;
 }
 console.log(book.info());
-// Exercise 3 Section for (let i = 0; i < nums.length; i++) {} 
 
-let sentence = "This quick brown fox jumps over the lazy dog";
+/* Exercise 3 Section  Reverse ever word in a sentence */
+let sentence = "The quick brown fox jumps over the lazy dog";
+
+function reverseWords(sentence) {
+    let result = [];
+    // Identify the words based on spaces
+let words = sentence.split(" ");
+for (let i = 0; i < words.length; i++) {
+ let letters = words[i].split("") // ex: "The" ["T", "h","e"]
+ letters.reverse(); // ex: ["e", "h", "T"]
+ let reverseWord = letters.join("");
+ result.push(reverseWord); // adds the given value to the end of the array
+    }
+
+ return result.join(" ");
+
+}
+
+console.log(reverseWords(sentence));
+
+// Exercise 4  (can be) String .split (can be) Arrays .join
+
+let csvData = "name,age\nFrodo,50\nSam,38\nMerry,36\nPippin,26";
+
+function parseCSV(csvString) {
+const lines = csvData.split("\n");
+console.log(lines);
+
+
+const headers = lines[0].split(",");
+console.log(headers);
+const resultArray = [];
+for (let i = 1; i< lines.length; i++) {
+    const data = lines[i].split(",");
+    console.log(data);
+    const temObj = {
+        name: data[0],
+        age: [1]
+    }
+     resultArray.push(tempObj);
+}
+console.log(resultArray);
+return resultArray;
+}
+
+console.log(parseCSV(csvData));
+
+
+
+
+
+
+
+
+
+
+
+
+
